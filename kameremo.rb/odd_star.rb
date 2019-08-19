1d302bd8c0a1c53d2aad162561a59c67001f748f
slintristeza <slintristeza@gmail.com>
2019-08-19 22:55:07 +0900

    ref:apply formater

--- kameremo.rb/odd_star.rb
+++ kameremo.rb/odd_star.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 n = 21
-ex_angle = 180 - 180/n
+ex_angle = 180 - 180 / n
 
 pen_down
 n.times do
   forward(Math::PI * 300 / n)
-  turn_left (ex_angle)
+  turn_left ex_angle
 end
