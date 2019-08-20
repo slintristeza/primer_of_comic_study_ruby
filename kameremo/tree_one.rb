cbcbb4c29ed1c5a9f1cde677d381b56dee52ea5d
slintristeza <slintristeza@gmail.com>
2019-08-20 23:42:55 +0900

    ref:Standardization of processes

--- kameremo/tree_one.rb
+++ kameremo/tree_one.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
-def draw_tree(length)
+def draw_tree1(length)
   forward(length * 0.5)
   turn_left 35
-  forward(length * 0.8)
-  backward(length * 0.8)
+  draw_tree0(length * 0.8)
   turn_right 60
-  forward(length * 0.6)
-  backward(length * 0.6)
+  draw_tree0(length * 0.6)
   turn_left 25
   backward(length * 0.5)
+end
+
+def draw_tree0(length)
+  forward length
+  backward length
 end
 
 move_to 0, 140
 pen_down
 
-draw_tree 250
+draw_tree1 250
