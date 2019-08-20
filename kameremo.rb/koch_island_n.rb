3ac5c74a5650d317b9ca9ecf8941acd296d45b51
slintristeza <slintristeza@gmail.com>
2019-08-20 12:44:18 +0900

    feat:koch_island_n

---
+++ kameremo.rb/koch_island_n.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+def draw_koch(n, length)
+  if n == 0
+    forward(length)
+  else
+    draw_koch(n - 1, length / 3)
+    turn_left 60
+    draw_koch(n - 1, length / 3)
+    turn_right 120
+    draw_koch(n - 1, length / 3)
+    turn_left 60
+    draw_koch(n - 1, length / 3)
+  end
+end
+
+move_to(-120, 80)
+turn_right 30
+pen_down
+
+3.times do
+  draw_koch(5, 270) #(次数, 線分の長さ)
+  turn_right 120
+end
